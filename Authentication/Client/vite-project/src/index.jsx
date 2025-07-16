be7ca323a51f5b2de3e1b92f405f39c9cef1e7de
RAJESH <rmurugan3174@gmail.com>
2025-07-16 23:52:06 +0530

    Authentication Project

---
+++ Authentication/Client/vite-project/src/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import Register from "./register";
+import Login from "./login";
+import Welcome from "./welcome";
+
+const Display=createBrowserRouter([
+    {
+        path:'/register',
+        element:<Register />
+    },
+    {
+        path:'/login',
+        element:<Login />
+    },
+    {
+      path:'/welcome',
+      element:<Welcome />  
+    }
+])
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<RouterProvider router={Display}/>)
